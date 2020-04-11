9ab3a4deb317c1b89ec9b5399e900dea5a4f6bf3
Alex Makarevich <lemas096@gmail.com>
2020-04-11 10:04:30 +0300

    feat: complete subtasks 1,2,3

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -1,25 +1,16 @@
 package subtask3
 
-import java.lang.StringBuilder
 
 class Abbreviation {
 
     fun abbreviationFromA(a: String, b: String): String {
-        val final = StringBuilder()
-        val aTemp = a.toUpperCase()
-        b.forEachIndexed { indexb, charb ->
-            aTemp.forEachIndexed { indexa, chara ->
-                if (indexa >= indexb) {
-                    when (chara) {
-                        charb -> {
-                            final.append(charb)
-                        }
-                    }
-                }
-            }
-        }
-        return if (final.toString() == b) {
-            "YES"
-        } else "NO"
+        return if (a.contains(b, true) || a.filter { b.contains(it, true) }.equals(b, true)) "YES"
+        else "NO"
     }
+
 }
+
+
+
+
+
