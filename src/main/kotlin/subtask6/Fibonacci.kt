107a252413b16a00bfea51eda44fb5c96a9b095f
Alex Makarevich <lemas096@gmail.com>
2020-04-13 18:03:21 +0300

    feat: complete subtasks 1upd, 2, 3, 4, 5, 6

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -4,6 +4,26 @@ class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        val finalArray: IntArray = IntArray(3)
+        val fibonacci = arrayOf(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377)
+
+        for (i in 1 until  fibonacci.size){
+            if (fibonacci[i]*fibonacci[i-1] <= n && fibonacci[i]*fibonacci[i+1] > n){
+                if (fibonacci[i]*fibonacci[i-1] == n) {
+                    finalArray[0] = fibonacci[i-1]
+                    finalArray[1] = fibonacci[i]
+                    finalArray[2] = 1
+                }
+                else {
+                    finalArray[0] = fibonacci[i]
+                    finalArray[1] = fibonacci[i+1]
+                    finalArray[2] = 0
+                }
+            }
+        }
+
+        finalArray.forEach { println(it) }
+
+return finalArray
     }
 }
