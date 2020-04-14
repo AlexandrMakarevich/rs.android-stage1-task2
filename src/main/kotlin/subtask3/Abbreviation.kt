f88b0ccce6a6f851738c53df62beeda971efc518
Alex Makarevich <lemas096@gmail.com>
2020-04-14 18:06:51 +0300

    feat: complete subtasks 1upd, 2upd, 3upd, 4, 5upd_, 6upd

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -1,33 +1,7 @@
 package subtask3
 
 import java.lang.StringBuilder
-class Abbreviation {
-    var result = false
 
-    fun check(a: String, b: String, i: Int, j: Int){
-        if(j == b.length){
-            result = true
-            return
-        }
-        if(i == a.length) return
-        if(result) return
-        else{
-            if(a[i] == b[j]) check(a, b, i+1, j+1)
-            else if(a[i] == b[j].toLowerCase()){
-                check(a, b, i+1, j+1)
-                check(a, b, i+1, j)
-            }
-            else if(a[i].isUpperCase()) return
-            else check(a, b, i+1, j)
-        }
-    }
-
-    fun abbreviationFromA(a: String, b: String): String {
-        check(a, b, 0, 0)
-        return if(result) "YES" else "NO"
-    }
-}
-/*
 class Abbreviation {
 
     fun abbreviationFromA(a: String, b: String): String {
@@ -49,7 +23,6 @@ class Abbreviation {
         } else "NO"
     }
 }
-*/
 
 
 
