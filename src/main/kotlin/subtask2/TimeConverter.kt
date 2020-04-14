a65a67e205094b045f2f4ec6f2f769e64077d150
Alex Makarevich <lemas096@gmail.com>
2020-04-14 18:00:58 +0300

    feat: complete subtasks 1upd, 2upd, 3, 4, 5upd_, 6

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -2,6 +2,33 @@ package subtask2
 
 class TimeConverter {
 
+    fun toTextFormat(hour: String, minute: String): String {
+        val numbers = arrayOf(
+            "one", "two", "three", "four",
+            "five", "six", "seven", "eight",
+            "nine", "ten", "eleven", "twelve",
+            "thirteen", "fourteen", "fifteen",
+            "sixteen", "seventeen",
+            "eighteen", "nineteen", "twenty"
+        )
+        val intHour = hour.toInt()
+        val intMinute = minute.toInt()
+        return when {
+            intMinute > 60 -> ""
+            intMinute == 0 -> numbers[intHour - 1] + " o' clock"
+            intMinute == 15 -> "quarter past " + numbers[intHour - 1]
+            intMinute == 30 -> "half past " + numbers[intHour - 1]
+            intMinute == 45 -> "quarter to " + numbers[intHour]
+            intMinute <= 20 -> numbers[intMinute - 1] + " minutes past " + numbers[intHour - 1]
+            intMinute < 30 -> "twenty " + numbers[intMinute % 20 - 1] + " minutes past " + numbers[intHour - 1]
+            intMinute <= 40 -> "twenty " + numbers[(60 - intMinute) % 20 - 1] + " minutes to " + numbers[intHour]
+            else -> numbers[(60 - intMinute) - 1] + " minutes to " + numbers[intHour]
+        }
+    }
+}
+/*
+class TimeConverter {
+
     // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
 
@@ -45,3 +72,4 @@ class TimeConverter {
         return "unknown"
     }
 }
+*/
--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -4,65 +4,6 @@ import java.util.*
 import kotlin.text.StringBuilder
 
 class Pangram {
-    private val vowels = arrayOf('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u', 'y', "Y")
-
-    private fun isPangram(s:String):Boolean{
-        val letters = Array<Boolean>(26){false}
-        for(i in s.indices){
-            if(s[i].isLetter())
-                letters[s[i].toLowerCase() - 'a'] = true
-        }
-        return !letters.contains(false)
-    }
-
-    private fun vowelsCount(s: String): Int{
-        var count = 0
-        for(i in s.indices){
-            if(vowels.contains(s[i])){
-                count++
-            }
-        }
-        return count
-    }
-
-    private fun consonantsCount(s: String): Int{
-        var count = 0
-        for(i in s.indices){
-            if(s[i].isLetter() && !vowels.contains(s[i])){
-                count++
-            }
-        }
-        return count
-    }
-    fun getResult(inputString: String): String{
-        val pangram = isPangram(inputString)
-        val s = inputString.split(" ").filter { it.isNotEmpty() && it[0] !='\n' }.sortedBy {
-            if(pangram) vowelsCount(it)
-            else consonantsCount(it)
-        }
-        val result = ArrayList<String>()
-        s.forEach(){
-            val sb = StringBuilder("")
-            var count = 0
-            for(c in it){
-                if(pangram){
-                    if(vowels.contains(c)) {
-                        count++
-                        sb.append(c.toUpperCase())
-                    }else sb.append(c)
-                }
-                else if(c.isLetter() &&  !vowels.contains(c)) {
-                    count++
-                    sb.append(c.toUpperCase())
-                }else sb.append(c)
-            }
-            sb.insert(0, count.toString())
-            result.add(sb.toString())
-        }
-        return result.joinToString(" ")
-    }
-}
-/*class Pangram {
     val vowels = arrayOf('a', 'e', 'i', 'o', 'u', 'y')
     val odd = 'b'..'z' subtract (vowels.toList())
 
@@ -225,4 +166,4 @@ class Pangram {
 
         return finalString.toString().dropLast(1)
     }
-}*/
+}
