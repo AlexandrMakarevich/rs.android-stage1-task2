9b34ab9f30e6d875773cb7ccd568991337f190af
Alex Makarevich <lemas096@gmail.com>
2020-04-14 18:09:17 +0300

    feat: complete 1, 2, 3, 4, 5, 6

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -4,21 +4,6 @@ class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        val result = IntArray(3){1}
-        var t = 0
-        while(result[0] * result[1] < n){
-            t = result[0] + result[1]
-            result[0] = result[1]
-            result[1] = t
-        }
-        if(result[0] * result[1] != n) result[2] = 0
-        return result
-    }
-}
-/*class Fibonacci {
-
-    // TODO: Complete the following function
-    fun productFibonacciSequenceFor(n: Int): IntArray {
         val finalArray: IntArray = IntArray(3)
         val fibonacci = arrayOf(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377)
 
@@ -41,4 +26,4 @@ class Fibonacci {
 
 return finalArray
     }
-}*/
+}
